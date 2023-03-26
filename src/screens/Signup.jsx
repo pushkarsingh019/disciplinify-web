343edfb72c889763610831055e61851aff2e59e4
Pushkar Singh <stoazest@gmail.com>
2023-03-26 16:59:33 +0530

    signup screen ui done

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function Signup() {
   return (
-    <div className="screen">
-      <h1>Signup</h1>
+    <div className="signup-screen">
+      <form className="form">
+        <h2>Create an account</h2>
+        <input type="text" placeholder="Name" required />
+        <br />
+        <input type="email" placeholder="Email" required />
+        <br />
+        <input type="password" placeholder="Password" required />
+        <br />
+        <button type="submit" className="cta">
+          Sign up
+        </button>
+      </form>
+      <br />
+      <br />
+      <small>
+        Already have an account ? <Link to={`/login`}>Login</Link>
+      </small>
     </div>
   );
 }
