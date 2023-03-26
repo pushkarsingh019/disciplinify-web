6661a3357cec0e71914a9a67aec975bff3640179
Pushkar Singh <stoazest@gmail.com>
2023-03-26 10:13:50 +0530

    first commit

---
+++ serviceworker.js
@@ -0,0 +1,41 @@
+// service worker for disciplinify
+
+const cacheName = "version-1";
+const urlsToCache = ['./index.html', './offline.html'];
+
+const self = this;
+
+// installing service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then((cache) => {console.log("opened cache"); return cache.addAll(urlsToCache)})
+    )
+})
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('./offline.html'))
+            })
+    )
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(cacheName);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+});
