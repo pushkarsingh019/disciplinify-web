69714c8bcf184fcf0f213f6d78f314b22fc15459
Pushkar Singh <stoazest@gmail.com>
2023-03-26 17:59:48 +0530

    env varaibles

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,7 +1,7 @@
 // store for the application
 import {createStore, action, thunk} from "easy-peasy";
 import axios from "axios";
-const backendUrl = "http://localhost:8080"
+const backendUrl = process.env.origin || "http://localhost:8080"
 
 const store = createStore({
     userData : {},
